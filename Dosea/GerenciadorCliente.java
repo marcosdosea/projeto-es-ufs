7674b84cb34a99bb57f75d35ec6bf311e2883f75
marcosdosea@gmail.com <marcosdosea@gmail.com@535ce3a6-7f45-f405-f5f3-f03d800e99b5>
2014-05-05 12:56:20 +0000

    nova variável corrigindo o problema.

--- Dosea/GerenciadorCliente.java
+++ Dosea/GerenciadorCliente.java
@@ -1,7 +1,9 @@
 public class gerenciadorCliente{
 	String cliente = null;
 	public String getCliente(){
+	    string nome = "Dosea";
 		int x = 0;
+        int z = 2;
 		return cliente;
 	}
 	public void setCliente(String name){
