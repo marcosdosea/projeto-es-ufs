5ee88d4bffa151922b951270d648dd08236ba342
marcosdosea@gmail.com <marcosdosea@gmail.com@535ce3a6-7f45-f405-f5f3-f03d800e99b5>
2014-05-05 14:06:11 +0000

    alterações em andamento

--- Dosea/GerenciadorCliente.java
+++ Dosea/GerenciadorCliente.java
@@ -1,10 +1,6 @@
 public class gerenciadorCliente{
 	String cliente = null;
 	public String getCliente(){
-	    string nome = "Dosea";
-		int x = 0;
-        int z = 2;
-		nome = "Rafael";
 		int w = 23;
 		return cliente;
 	}
