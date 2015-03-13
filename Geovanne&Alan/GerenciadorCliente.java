67b0845433a662bb710160aee421d2568a660c73
alanpassossi@gmail.com <alanpassossi@gmail.com@535ce3a6-7f45-f405-f5f3-f03d800e99b5>
2015-03-13 12:00:10 +0000

    class atualizada GerenciadorCliente.java

--- Geovanne&Alan/GerenciadorCliente.java
+++ Geovanne&Alan/GerenciadorCliente.java
@@ -2,13 +2,12 @@ public GerenciadorCliente{
 	
 	private String nomeCliente = null;
 	
-	public void inserirOla(String nomeCliente){
-		
-	
+	public void inserir(String nomeCliente){
+	// metodo para inserir cliente
 		this.nomeCliente = nomeCliente;
 	}
 	 public atualizar(){
-		 
+		 //metodo de atualizaçao de cliente
 		 string nome;
 		 
 	 }
