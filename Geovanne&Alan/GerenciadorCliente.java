9a6748bc1cfedcb5995092f796105ff879f08078
geovannesdi@gmail.com <geovannesdi@gmail.com@535ce3a6-7f45-f405-f5f3-f03d800e99b5>
2015-03-13 13:23:41 +0000

    Marcação de branches Geovanne Alan

--- Geovanne&Alan/GerenciadorCliente.java
+++ Geovanne&Alan/GerenciadorCliente.java
@@ -2,22 +2,14 @@ public GerenciadorCliente{
 	
 	private String nomeCliente = null;
 	
-	public void inserirOla(String nomeCliente){
-		//comentario aqui pra dar conflito 
-	public void inserir(String nomeCliente){
-		//teste de comentario para conflitos
-		String variavel;
+	public void inserir (String nomeCliente){
+
+	
 		this.nomeCliente = nomeCliente;
 	}
 	 public atualizar(){
-		 //metodo de atualizaçao de cliente
+		 
 		 string nome;
-		 
-	 }
-	 
-	 plubic void(){
-		 
-		 //teste ola 
 		 
 	 }
 		
--- Luiz e Bárbara/GerenciadorCliente.java
+++ Luiz e Bárbara/GerenciadorCliente.java
@@ -1,13 +1,8 @@
 public class GerenciadorCliente{
-  
    public String inserir(String nome){
-	//adicionar um novo cliente
-	nomeCliente.add(nome);
 
    }
-
    public String atualizar(String nome){
-     return nome;
 
    }
 }
