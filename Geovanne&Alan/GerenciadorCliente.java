24e492b50b8db78c148b498d808917bbc4144358
geovannesdi@gmail.com <geovannesdi@gmail.com@535ce3a6-7f45-f405-f5f3-f03d800e99b5>
2015-03-13 12:02:33 +0000

    Modificação da class

--- Geovanne&Alan/GerenciadorCliente.java
+++ Geovanne&Alan/GerenciadorCliente.java
@@ -2,6 +2,8 @@ public GerenciadorCliente{
 	
 	private String nomeCliente = null;
 	
+	public void inserirOla(String nomeCliente){
+		//comentario aqui pra dar conflito 
 	public void inserir(String nomeCliente){
 	// metodo para inserir cliente
 		this.nomeCliente = nomeCliente;
