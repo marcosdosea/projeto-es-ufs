5a9573ddef1baf05a0256c7a5bbc817e41ec50db
alanpassossi@gmail.com <alanpassossi@gmail.com@535ce3a6-7f45-f405-f5f3-f03d800e99b5>
2015-03-13 11:42:04 +0000

    criado o método inserircliente2 na class GerenciadorCliente.java

--- Geovanne&Alan/GerenciadorCliente.java
+++ Geovanne&Alan/GerenciadorCliente.java
@@ -1,19 +1,16 @@
 public GerenciadorCliente{
 	
-<<<<<<< .mine
-=======
 	private String nomeCliente = null;
->>>>>>> .r288
-		
-<<<<<<< .mine
-		
-=======
 	public void inserir(String nomeCliente){
->>>>>>> .r288
+
 	
 		this.nomeCliente = nomeCliente;
 	}
-	
+	 public inserircliente2(){
+		 
+		 
+		 
+	 }
 		
 	
 	
